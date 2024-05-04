c7095f4d1acc6f9cec051a2aaafc80a6334c369c
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-05-04 14:09:17 +0300

    crating tables after connecting with db

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,20 @@ sequelize
   .catch((err) => console.error("Error connecting to MySQL:", err));
 
 
+// import ur model in starter point of ur app sync models with the database
+const User = require("./models/User");
+const Product = require("./models/Product");
+const Category = require("./models/Category");
+const Rating = require("./models/Rating");
+
+(async () => {
+  try {
+    await sequelize.sync({ force: true }); // set force to true to drop existing tables
+    console.log("Database tables created successfully");
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
+})();
 
 
 
